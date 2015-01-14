eb9034fb073695886f8a0a3afc159c2f3cd6a6b9
Matthew Boyd <2mjboyd@gmail.com>
2015-01-14 14:37:47 -0800

    changes angles from 0-180 to 0-360

--- modules/motor/motor.js
+++ modules/motor/motor.js
@@ -8,62 +8,65 @@ function Motor(model_ref, feedback) {
 	this.model = model_ref;
 	this.feedback = feedback;
 }
-Motor.prototype.setDirection=function(left, Right, Dir){ //Sets Motors Forward or Directon 
-		if(left==(1*Dir)) {
-			Bone.digitalWrite('P9_13', Bone.HIGH);
-			Bone.digitalWrite('P9_15', Bone.HIGH);
-			Bone.digitalWrite('P9_12', Bone.HIGH);
+Motor.prototype.setDirection=function(left, Right){ //Sets Motors Forward or Directon 
+		if(left==(1)) {
+			BONE.digitalWrite('P9_13', BONE.HIGH);
+			BONE.digitalWrite('P9_15', BONE.HIGH);
+			BONE.digitalWrite('P9_12', BONE.HIGH);
 		}
-		else if(left==(-1*Dir)){
-			Bone.digitalWrite('P9_14', Bone.LOW);
-			Bone.digitalWrite('P9_16', Bone.LOW);
-			Bone.digitalWrite('P9_18', Bone.LOW);
+		else if(left==(-1)){
+			BONE.digitalWrite('P9_14', BONE.LOW);
+			BONE.digitalWrite('P9_16', BONE.LOW);
+			BONE.digitalWrite('P9_18', BONE.LOW);
 		}
-		if(Right==(1*Dir)){
-			Bone.digitalWrite('P9_13', Bone.HIGH);
-			Bone.digitalWrite('P9_15', Bone.HIGH);
-			Bone.digitalWrite('P9_12', Bone.HIGH);
+		if(Right==(1)){
+			BONE.digitalWrite('P9_13', BONE.HIGH);
+			BONE.digitalWrite('P9_15', BONE.HIGH);
+			BONE.digitalWrite('P9_12', BONE.HIGH);
 		}
-		else if(Right==(-1*Dir)){
-			Bone.digitalWrite('P9_14', Bone.LOW);
-			Bone.digitalWrite('P9_16', Bone.LOW);
-			Bone.digitalWrite('P9_18', Bone.LOW);
+		else if(Right==(-1)){
+			BONE.digitalWrite('P9_14', BONE.LOW);
+			BONE.digitalWrite('P9_16', BONE.LOW);
+			BONE.digitalWrite('P9_18', BONE.LOW);
 		}
 	}
 Motor.prototype.setSpeed=function(Left, Right){
-	Bone.analogWrite('P8_14', Left/100, 2000, printJSON);
-	Bone.analogWrite('P8_16', Left/100, 2000, printJSON);
-	Bone.analogWrite('P9_13', Left/100, 2000, printJSON);
-	Bone.analogWrite('P9_19', Right/100, 2000, printJSON);
-	Bone.analogWrite('P9_34', Right/100, 2000, printJSON);
-	Bone.analogWrite('P9_36', Right/100, 2000, printJSON);
+	BONE.analogWrite('P8_14', Left/100, 2000, console.log);
+	BONE.analogWrite('P8_16', Left/100, 2000, console.log);
+	BONE.analogWrite('P9_13', Left/100, 2000, console.log);
+	BONE.analogWrite('P9_19', Right/100, 2000, console.log);
+	BONE.analogWrite('P9_34', Right/100, 2000, console.log);
+	BONE.analogWrite('P9_36', Right/100, 2000, console.log);
+	this.feedback = "Left speed" + Left + "Right speed" + Right;
 }
 Motor.prototype.set=function( angle, speed){
-		var setSpeedLeft, setSpeedRight;
-		var Direction=1;
-		if(speed<0) {Direction=-1; speed=speed*-1}; //puts the rover in reverse
-		if(angle==0){ // checks for angle 0
-			this.setSpeed(speed,speed);
-			this.setDirection( 1,-1, Direction);
-			this.feedback = "Left speed" + speed + "Right speed" + speed;
+		if(angle==0 || angle==360){ // checks for angle 0
+			this.setSpeed(speed,0);
+			this.setDirection( 1,1);
 		}
 		else if(angle<=90 && angle>0){ // checks for angles 1-90
 			this.setSpeed(speed,(angle/90)*speed);
-			this.setDirection( 1, 1, Direction);
-			this.feedback = "Left speed" + ((angle/90)*speed) + "Right speed" + speed;
+			this.setDirection( 1, 1);
 		}
 		else if(angle>90 && angle<180){ // checks for angles 91-179
 			this.setSpeed(((-1*(angle-180))/90)*speed,speed);
-			this.setDirection( 1, 1, Direction);
-			this.feedback = "Left speed" + speed + "Right speed" + ((-1*(angle-180))/90)*speed ;
+			this.setDirection( 1, 1);
 		}
 		else if(angle==180){ // checks for angle 180
-			this.setSpeed(speed,speed);
-			this.setDirection( -1, 1, Direction);
-			this.feedback = "Left speed" + speed + "Right speed" + speed;
+			this.setSpeed(0,speed);
+			this.setDirection( 1, 1);
+		}
+		else if (angle>180 && angle <=270){
+			this.setSpeed(((angle-180)/90)*speed,speed);
+			this.setDirection( -1, -1);
+		}
+		else if (angle>270 && angle <360){
+			this.setSpeed(speed,((-1*(angle-360))/90)*speed);
+			this.setDirection( -1, -1);
 		}
 		else{
-			this.feedback = "Could Not Interpret Command";
+			this.setSpeed(0,0);
+			this.feedback = "Could Not Interpret Command, Rover was STOPPED";
 		}
 	}
 Motor.prototype.handle = function(data) {
