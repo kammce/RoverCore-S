a17c8c45ab6ec267c690ca418b762ee26e45a4d5
Khalil Estell <khalil.estell@sjsu.edu>
2015-01-17 18:24:38 -0800

    Moving code to just be a uart handler

--- modules/spine.js
+++ modules/spine.js
@@ -3,33 +3,72 @@
 function Spine(feedback) {
 	this.os = require('os');
 	this.hardware = {
-		pwm: {
+		pwms: {
 			pins: ["P8_13", "P8_13", "P8_19", "P8_34", "P8_36", "P9_28", "P9_29"],
 			path: function(pin) {
 				return "/sys/devices/ocp.3/pwm_test_"+pin+".*/";
 			}
 		},
-		uart: [ "/dev/ttyO1", "/dev/ttyO2", "/dev/ttyO4", "/dev/ttyO5" ],
-		gpio: 
+		uarts: [ "/dev/ttyO1", "/dev/ttyO2", "/dev/ttyO4", "/dev/ttyO5" ],
+		gpios: {
+			exposed: [],
+			path: function(gpio) {
+				return "/sys/class/gpio/gpio"+gpio;
+			},
+
+		}
+	}
+	this.pinMap = {
 
 	}
 
-
 	if(this.os == 'beaglebone') {
 		console.log("System Hostname is Beaglebone");
-		console.log("Checking if PWM and UARTS are enabled");
-
-		console.log("Check failed! Please add this line to the uEnv.txt file:");
-		console.log("\toptargs=quiet drm.debug=7 capemgr.enable_partno=BB-UART1,BB-UART2,BB-UART4,BB-UART5,am33xx_pwm,bone_pwm_P8_13,bone_pwm_P8_19,bone_pwm_P8_34,bone_pwm_P8_36,bone_pwm_P9_28,bone_pwm_P9_29");
-
- 		console.log("Initializing GPIOs");
-		console.log("Configuring & Enabling PWMS (defaults to 0 duty cycle)");
+		console.log("Checking if UARTS are enabled");
 	} else {
 		console.log("Running on none Beagblebone platform.");
 	}
 }
-Spine.prototype.handle = function(data) {
-	console.log("No current handlers for Spine, found", data);
+Spine.prototype.pinMap = function(pin, ) {
+	
 }; 
+Spine.prototype.pinMode = function(pin, ) {
+	
+}; 
+Spine.prototype.digitalRead = function(pin) {
+	var gpio = this.pinmap(pin);
+	if(_.isUndefined(gpio)) {
+		console.log("Invalid PIN "+pin);
+		return false;
+	} 
+	var path = this.hardware.gpios.path(gpio);
+	var contents = fs.readFileSync(path);
+	if(_.isUndefined(contents)) {
+		console.log("Could not read PIN "+pin);
+	} else {
+		return contents;
+	}	
+}; 
+Spine.prototype.digitalWrite = function(pin, data) {
+	if(!_.isNumber(data) || ) {
+		console.log("Invalid PIN "+pin);
+		return false;
+	}
+	var gpio = this.pinmap(pin);
+	if(_.isUndefined(gpio)) {
+		console.log("Invalid PIN "+pin);
+		return false;
+	} 
+	var path = this.hardware.gpios.path(gpio);
+	var contents = fs.readWriteSync(path, data+"");
+	if(_.isUndefined(contents)) {
+		console.log("Could not read PIN "+pin);
+	} else {
+		return contents;
+	}	
+}; 
+Spine.prototype.uartSend = function(uart, msg) {
+	
+};
 
 module.exports = exports = MindController;
