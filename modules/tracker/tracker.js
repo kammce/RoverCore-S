95fa7a9f5d2933aee77cf781710e1ed585beea05
Mitchell Waldman <mitchswaldman@gmail.com>
2015-01-20 11:44:28 -0800

    Added tracker functionality for setting camera zoom

--- modules/tracker/tracker.js
+++ modules/tracker/tracker.js
@@ -7,6 +7,9 @@ Tracker.prototype.constructor = Tracker;
 function Tracker(model_ref, feedback) {
 	this.model = model_ref;
 	this.feedback = feedback;
+	this.pitch = 0;
+	this.yaw = 0;
+	this.zoom = 1;
 }
 Tracker.prototype.handle = function(data) {
 	console.log(this.module+" Recieved ", data);
@@ -14,7 +17,9 @@ Tracker.prototype.handle = function(data) {
 		this.setRotations(data);
 	} else if (data["req"] == "Get Rotations") {
 		return this.getRotations();
-	} 
+	}  else if (data["req"] == "Set Zoom") {
+		this.setZoom(data);
+	}
 };
 
 Tracker.prototype.resume = function() {};
@@ -29,6 +34,11 @@ Tracker.prototype.setRotations = function(data) {
 
 Tracker.prototype.getRotations = function() {
 	return { pitch : this.pitch , yaw : this.yaw };
+}
+
+Tracker.prototype.setZoom = function(data) {
+	console.log("Setting zoom");
+	this.zoom = data["zoom"];
 }
 
 module.exports = exports = Tracker;
