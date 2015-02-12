71333d5eb68c78bd696885d6a6b10d9e6eca85b3
Khalil Estell <khalil.estell@sjsu.edu>
2015-02-11 18:12:00 -0800

    Fixing variable name typo on spine.js

--- modules/spine.js
+++ modules/spine.js
@@ -276,7 +276,7 @@ Spine.prototype.setPWM = function(_pin, percent) {
 	};
 	var path = this.hardware.pwms[map[_pin]];
 	if(typeof path == "undefined") {
-		console.log("Invalid pwm pin: "+pin);  
+		console.log("Invalid pwm pin: "+_pin);  
 		return;
 	}
 	var maxnsec = 500000;
