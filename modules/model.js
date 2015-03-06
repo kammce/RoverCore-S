9573ae6ea50e3ca5c1588aa3671496582e330cb3
Huy Mai <kurst811@gmail.com>
2015-03-06 22:02:09 +0000

    model.js edited online with Bitbucket

--- modules/model.js
+++ modules/model.js
@@ -1,7 +1,48 @@
 "use strict";
 
 var Model = {
-	gravity: 0,
-	distance: 20.1
+	
+
+	gyro:{
+		x:0,
+		y:0,
+		z:0
+		},	
+
+	accelero:{
+		x:0,			
+		y:0,
+		z:0
+		},
+
+	compass:{							
+		x:0,			
+		y:0,
+		z:0
+		}, 
+ 	
+ 	GPS:{
+		altitude:0,
+		logitude:0,
+		latitude:0
+		},
+
+	 Power:{
+	 	voltage:0
+		},
+
+		wheel_speed:[0,0,0,0,0,0]
+
+	heading:0,
+
+	pitch_gyro:0,
+	yaw_gyro:0,
+	roll_gyro:0,
+
+	pitch_accelero:0,
+	yaw_accelero:0,
+	roll_accelero:0		
+
 }
+
 module.exports = exports = Model;
