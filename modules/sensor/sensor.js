1f8538f2c528b476b7e0252e846941fd41e20936
Huy Mai <kurst811@gmail.com>
2015-03-07 01:01:41 +0000

    sensor.js edited online with Bitbucket

--- modules/sensor/sensor.js
+++ modules/sensor/sensor.js
@@ -1,24 +1,7 @@
-
-
 "use strict";
 
-
 var Skeleton = require("../skeleton.js");
-var zeroOffset = 0.06957;		//constant for g-value
-var conversionFactor = 0.4584;	//constant for g-valuehttps://www.sparkfun.com/datasheets/Sensors/Accelerometer/ADXL345.pdf
-var Lsb = 14.375; 	//constant use to get true rad/s
-
 var ADXL345 = require('./index.js');
-var i2c = require('i2c'); 
-var port = '/dev/i2c-2';  
-      //i2c bus
-var address_gyroscope = 0x68;    //address of gyroscope
-var address_accelerometer = 0x53; //address of accelerometer
-var address_compass = 0x1e; //address of compass
-var compass_stop;
-
-
-
 
 Sensor.prototype = new Skeleton("SENSOR");
 Sensor.prototype.constructor = Sensor;
@@ -37,7 +20,7 @@ Sensor.prototype.handle = function (data) {				// take command from user interfa
 		this.compass();
 		return "gyro:  pitch:" + this.model.pitch_gyro + "  roll:" + this.model.roll_gyro + " yaw:"  + this.model.yaw_gyro
 		+ "  accelero:  pitch:" + this.model.pitch_accelero + "  roll:" + this.model.roll_accelero + " yaw:"  + this.model.yaw_accelero 
-		+ " Heading: " + this.model.heading + " degrees";
+		+ " Heading: " + this.model.compass.heading + " degrees";
 	}
 
 	if(data.start == "gyro"){
@@ -53,7 +36,7 @@ Sensor.prototype.handle = function (data) {				// take command from user interfa
 
 	if(data.start == "compass"){
 		this.compass();
-		return " Heading: " + this.model.heading + " degrees" ;
+		return " Heading: " + this.model.compass.heading + " degrees" ;
 	}
 
 	if(data.stop == "all"){
@@ -74,7 +57,8 @@ Sensor.prototype.update = function() {
 
 Sensor.prototype.compass = function() {                 // degrees refer to North
 
-   	var wire = new i2c(address_compass, {device: '/dev/i2c-2'});
+	var address_compass = 0x1e; //address of compass
+   	var wire = new I2C(address_compass, {device: '/dev/i2c-2'});
 
    	wire.writeBytes( 0x00, [0x70], function(err){});
 	wire.writeBytes( 0x01, [0xA0], function(err){});   	
@@ -118,21 +102,21 @@ Sensor.prototype.compass = function() {                 // degrees refer to Nort
     heading = ((heading * 180)/pi); 
 
     if( heading >= 0 && heading <= 137){
-    	this.model.heading *= .6569 
+    	this.model.compass.heading *= .6569 
     }
 
     else if ( heading > 137 && heading <= 215){
-    	this.model.heading = ((heading-137)*1.16883117 + 90)
+    	this.model.compass.heading = ((heading-137)*1.16883117 + 90)
 
     }
 
      else if ( heading > 215 && heading <= 281){
-    	this.model.heading = ((heading-215)*1.3636 + 180)
+    	this.model.compass.heading = ((heading-215)*1.3636 + 180)
 
     }
 
     else if ( heading > 281 && heading <= 0){
-    	this.model.heading = ((heading-281)*1.3924 + 270)
+    	this.model.compass.heading = ((heading-281)*1.3924 + 270)
 
     }
 
@@ -147,7 +131,7 @@ Sensor.prototype.compass = function() {                 // degrees refer to Nort
 
     console.log("x: " + this.model.compass.x + " y: " + this.model.compass.y + " z: " + this.model.compass.z + " uT");    // Display the results 
                                                                                                     //(magnetic vector values are in micro-Tesla (uT))
-    console.log('Heading: ' + this.model.heading + ' degrees' );
+    console.log('Heading: ' + this.model.compass.heading + ' degrees' );
 
 
 		});
@@ -158,7 +142,8 @@ Sensor.prototype.compass = function() {                 // degrees refer to Nort
 
 Sensor.prototype.gyro= function(){
 
-	var wire = new i2c(address_accelerometer, {device: '/dev/i2c-1'});
+	var address_accelerometer = 0x53; //address of accelerometer
+	var wire = new I2C (address_accelerometer, {device: '/dev/i2c-2'});
 
 	wire.writeBytes(0x16, [1<<3 | 1<<4 | 1<<0 ], function(err) {}); // set rate 2000
 	wire.writeBytes(0x15, [ 0x09 ], function(err) {}); // set sample rate to 100hz
@@ -174,9 +159,9 @@ Sensor.prototype.gyro= function(){
 
 			// convert binary to signed decimal 
 
-			this.model.gyro.x = new Int16Array([res[0] << 8 | res[1]])[0]; //put binary into an array and called back the first number
-			this.model.gyro.z = new Int16Array([res[2] << 8 | res[3]])[0]; 
-			this.model.gyro.y = new Int16Array([res[4] << 8 | res[5]])[0];
+			var x = new Int16Array([res[0] << 8 | res[1]])[0]; //put binary into an array and called back the first number
+			var z = new Int16Array([res[2] << 8 | res[3]])[0]; 
+			var y = new Int16Array([res[4] << 8 | res[5]])[0];
 
 			}
 
@@ -186,11 +171,11 @@ Sensor.prototype.gyro= function(){
 
 			}
 
- 	this.model.pitch_gyro    =  (this.model.gyro.x)/14.375;    // 
-    this.model.roll_gyro     =  (this.model.gyro.y)/14.375;    //
-    this.model.yaw_gyro      =  (this.model.gyro.z)/14.375;    //
+ 	this.model.gyro.x    =  (x)/14.375;    // 
+    this.model.gyro.y    =  (y)/14.375;    //
+    this.model.gyro.z    =  (z)/14.375;    //
 
-    console.log("pitch: " + this.model.pitch_gyro + " roll: " + this.model.roll_gyro + " yaw: " + this.model.yaw_gyro + " degrees");
+    console.log("pitch: " + this.model.gyro.x + " roll: " + this.model.gyro.y + " yaw: " + this.model.gyro.z + " degrees");
 
 		});
 
@@ -201,11 +186,12 @@ Sensor.prototype.gyro= function(){
 
 Sensor.prototype.accelero = function(){
 
+var address_gyroscope = 0x68;    //address of gyroscope
 this.model.accelero.x = 0;
 this.model.accelero.y = 1;
 this.model.accelero.z = 2;
 
-var ADXL345 = require('./index.js');
+
 
 var globalvar = {
 	SAMPLECOUNT : 400,
