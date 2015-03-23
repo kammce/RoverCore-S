3359183e0e502338c02b21fc4b2a7bb0c8c986bb
Khalil Estell <kammcecorp@gmail.com>
2015-03-23 07:19:28 -0700

    Removing archived files... they are no longer needed

--- archieves/rj-mind-controller/mindcontrol-rj/funcExports/makeWriteables.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-function makeWrites(streamArray, pathArray){
-	for(var i = 0; i < streamArray.length; i++){
-		streamArray[i] = fs.createWriteStream(pathArray[i]);
-	}
-}
-
-module.exports = makeWrites;
