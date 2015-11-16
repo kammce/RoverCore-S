b8267c42b538dac9670ea1b44381b26e489f3606
khalil estell odroid <khalil.estell@sjsu.edu>
2015-11-16 17:30:26 +1030

    i2c.js experiments. discovered that setTimeout 0 across muliple timers yeld similar frequencies regardless of i2c

---
+++ experiments/i2c.js
@@ -0,0 +1,68 @@
+var i2c = require('i2c-bus');
+	i2c3 = i2c.openSync(3);
+
+var MUX_ADDR = 0x70;
+var IO_EXPANDER_ADDR = 0x41;
+var which = [true, true];
+var cmd = 0x00;
+// enable all channels
+cmd = [ 0xFF ];
+console.log(cmd.length);
+i2c3.sendByteSync(MUX_ADDR, 0xFF);
+cmd = [ 0x03, 0x00 ];
+i2c3.writeByteSync(IO_EXPANDER_ADDR, cmd[0], cmd[1]);
+
+var deter = true;
+
+cmd = 0x00;
+if(deter) {
+	setInterval(function() {
+		if(which[0]) {
+			cmd |= (0x01 << 0);
+		} else {
+			cmd &= ~(0x01 << 0);
+		}
+		i2c3.writeByteSync(IO_EXPANDER_ADDR, 0x01, cmd);
+		which[0] = !which[0];
+	}, 0);
+	setInterval(function() {
+		if(which[1]) {
+			cmd |= (0x01 << 1);
+		} else {
+			cmd &= ~(0x01 << 1);
+		}
+		i2c3.writeByteSync(IO_EXPANDER_ADDR, 0x01, cmd);
+		which[1] = !which[1];
+	}, 0);
+	setInterval(function() {
+		if(which[2]) {
+			cmd |= (0x01 << 2);
+		} else {
+			cmd &= ~(0x01 << 2);
+		}
+		i2c3.writeByteSync(IO_EXPANDER_ADDR, 0x01, cmd);
+		which[2] = !which[2];
+	}, 0);
+	setInterval(function() {
+		if(which[3]) {
+			cmd |= (0x01 << 3);
+		} else {
+			cmd &= ~(0x01 << 3);
+		}
+		i2c3.writeByteSync(IO_EXPANDER_ADDR, 0x01, cmd);
+		which[3] = !which[3];
+	}, 0);
+
+} else {
+	while(true) {
+		//setImmediate(function(){ 
+			if(which) {
+				cmd = 0x00;
+			} else {
+				cmd = 0xFF;
+			}
+			i2c3.writeByteSync(IO_EXPANDER_ADDR, 0x01, cmd);
+			which = !which;
+		//});
+	}
+}
