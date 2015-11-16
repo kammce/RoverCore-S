9615fbf14ab5414c34c40b784b51faefc475b7a2
khalil estell odroid <khalil.estell@sjsu.edu>
2015-11-16 16:01:40 +1030

    Changed attempt time for Rovercore

--- RoverCore.js
+++ RoverCore.js
@@ -3,14 +3,15 @@
 // Loading Libraries
 var Primus = require('primus');
 var Socket = new Primus.createSocket();
-var connection = Socket('http://localhost:9000');
+var connection = Socket('http://localhost:9000', { 
+	reconnect: {
+		max: 2000, // Number: The max delay before we try to reconnect.
+		min: 500, // Number: The minimum delay before we try reconnect.
+		retries: Infinity // Number: How many times we shoult try to reconnect.
+	}
+});
+
 var Cortex = require("./modules/Cortex");
 
-// Construct and start cortex
+// Construct and start Cortex
 new Cortex(connection);
-
-/*{ reconnect: {
-	max: 4000, // Number: The max delay before we try to reconnect.
-	min: 500, // Number: The minimum delay before we try reconnect.
-	retries: Infinity // Number: How many times we shoult try to reconnect.
-}*/
