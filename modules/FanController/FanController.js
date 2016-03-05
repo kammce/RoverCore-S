060996e3ad4642852b4d3b2bc87364cb94c563ce
Elizabeth Nguyen <liznguyen12@gmail.com>
2016-03-04 18:39:20 -0800

    Update: 20 failing test

--- modules/FanController/FanController.js
+++ modules/FanController/FanController.js
@@ -1,4 +1,4 @@
-"use strict";
+/*"use strict";
 
 var Neuron = require('../Neuron');
 
@@ -11,7 +11,7 @@ class FanController extends Neuron {
         this.idle_time = idle_timeout;
         this.i2c = i2c;
         this.model = model;
-        // Construct Class here
+        //Construct class here
     }
     react(input) {
         this.log.output(`REACTING ${this.name}: `, input);
@@ -38,10 +38,10 @@ class FanController extends Neuron {
         }
     }
     codeTEMP(value){
-        if(value>0){
-            return value.toString(16);
+        if(value<0){
+            return parseInt((value+256).toString(16),16);
         } else{
-            return (value+256).toString(16);
+            return parseInt(value.toString(16),16);
         }
     }
     readTemp() {
@@ -83,6 +83,153 @@ class FanController extends Neuron {
         i2c.writeByte(0x2C,0x54,LOWER);
         i2c.writeByte(0x2C,0x56,LOWER);
         var TEMPLIMIT = [];
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x45)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x47)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x49)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x4B)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x4D)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x4F)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x51)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x53)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x55)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x57)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x44)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x46)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x48)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x4A)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x4C)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x4E)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x50)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x52)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x54)));
+        TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x56)));
+        return TEMPLIMIT;
+    }  
+     setRegister() {
+        //ehhhhhh idk which ones I'm supposed to do. Gotta read that again
+     }
+     editRegister(CMD_ADDR,value) {
+        i2c.writeByte(0x2C,CMD_ADDR,value);
+        return i2c.readByte(0x2C,CMD_ADDR);
+     }
+     readRegister(CMD_ADDR) {
+        return i2c.readByte(0x2C,CMD_ADDR);
+     }
+}
+
+module.exports = FanController;
+*/
+"use strict";
+
+var Neuron = require('../Neuron');
+
+class FanController extends Neuron {
+    constructor(name, feedback, color_log, idle_timeout, i2c, model) {
+        super(name, feedback, color_log, idle_timeout);
+        this.name = name;
+        this.feedback = feedback;
+        this.log = color_log;
+        this.idle_time = idle_timeout;
+        this.i2c = i2c;
+        this.model = model;
+        this.TEMP = [];
+        this.TEMPLIMIT = [];
+        //Dummy vars just for testing purposes
+        this.r0x20 = 0x7F;
+        this.r0x21 = 0x7F;
+        this.r0x22 = 0x7F;
+        this.r0x23 = 0x7F;
+        this.r0x24 = 0x7F;
+        this.r0x25 = 0x7F;
+        this.r0x26 = 0x7F;
+        this.r0x27 = 0x7F;
+        this.r0x28 = 0x7F;
+        this.r0x29 = 0x7F;
+        this.r0x78 = 0x7F;
+        // Construct Class here
+    }
+    react(input) {
+        this.log.output(`REACTING ${this.name}: `, input);
+        this.feedback(this.name ,`REACTING ${this.name}: `, input);
+    }
+    halt() {
+        this.log.output(`HALTING ${this.name}`);
+        this.feedback(this.name ,`HALTING ${this.name}`);
+    }
+    resume() {
+        this.log.output(`RESUMING ${this.name}`);
+        this.feedback(this.name ,`RESUMING ${this.name}`);
+    }
+    idle() {
+        this.log.output(`IDLING ${this.name}`);
+        this.feedback(this.name ,`IDLING ${this.name}`);
+    }
+    decodeTEMP(temp) {
+        if(temp>=0x80){
+            return parseInt(temp,16)-256;
+        } 
+        else {
+            return parseInt(temp,16);
+        }
+    }
+    codeTEMP(value){
+        if(value>0){
+            return value.toString(16);
+        } else{
+            return (value+256).toString(16);
+        }
+    }
+    readTemp() {
+        var i2c = this.i2c;
+        /*
+        this.TEMP.push(decodeTEMP(i2c.readByte(0x2C,0x20)));
+        this.TEMP.push(decodeTEMP(i2c.readByte(0x2C,0x21)));
+        this.TEMP.push(decodeTEMP(i2c.readByte(0x2C,0x22)));
+        this.TEMP.push(decodeTEMP(i2c.readByte(0x2C,0x23)));
+        this.TEMP.push(decodeTEMP(i2c.readByte(0x2C,0x24)));
+        this.TEMP.push(decodeTEMP(i2c.readByte(0x2C,0x25)));
+        this.TEMP.push(decodeTEMP(i2c.readByte(0x2C,0x26)));
+        this.TEMP.push(decodeTEMP(i2c.readByte(0x2C,0x27)));
+        this.TEMP.push(decodeTEMP(i2c.readByte(0x2C,0x28)));
+        this.TEMP.push(decodeTEMP(i2c.readByte(0x2C,0x29)));
+        this.TEMP.push(decodeTEMP(i2c.readByte(0x2C,0x78)));
+        */
+        this.TEMP.push(this.decodeTEMP(0x80));
+        this.TEMP.push(this.decodeTEMP(i2c.readByteSync(0x2C,this.r0x21)));
+        this.TEMP.push(this.decodeTEMP(i2c.readByteSync(0x2C,this.r0x22)));
+        this.TEMP.push(this.decodeTEMP(i2c.readByteSync(0x2C,this.r0x23)));
+        this.TEMP.push(this.decodeTEMP(i2c.readByteSync(0x2C,this.r0x24)));
+        this.TEMP.push(this.decodeTEMP(i2c.readByteSync(0x2C,this.r0x25)));
+        this.TEMP.push(this.decodeTEMP(i2c.readByteSync(0x2C,this.r0x26)));
+        this.TEMP.push(this.decodeTEMP(i2c.readByteSync(0x2C,this.r0x27)));
+        this.TEMP.push(this.decodeTEMP(i2c.readByteSync(0x2C,this.r0x28)));
+        this.TEMP.push(this.decodeTEMP(i2c.readByteSync(0x2C,this.r0x29)));
+        this.TEMP.push(this.decodeTEMP(i2c.readByteSync(0x2C,this.r0x78)));
+        return this.TEMP;
+    }
+    writeTempLimits(UPPER_TEMP,LOWER_TEMP) {
+        var UPPER = codeTEMP(UPPER_TEMP);
+        var LOWER = codeTEMP(LOWER_TEMP);
+        i2c.writeByte(0x2C,0x45,UPPER);
+        i2c.writeByte(0x2C,0x47,UPPER);
+        i2c.writeByte(0x2C,0x49,UPPER);
+        i2c.writeByte(0x2C,0x4B,UPPER);
+        i2c.writeByte(0x2C,0x4D,UPPER);
+        i2c.writeByte(0x2C,0x4F,UPPER);
+        i2c.writeByte(0x2C,0x51,UPPER);
+        i2c.writeByte(0x2C,0x53,UPPER);
+        i2c.writeByte(0x2C,0x55,UPPER);
+        i2c.writeByte(0x2C,0x57,UPPER);
+        i2c.writeByte(0x2C,0x44,LOWER);
+        i2c.writeByte(0x2C,0x46,LOWER);
+        i2c.writeByte(0x2C,0x48,LOWER);
+        i2c.writeByte(0x2C,0x4A,LOWER);
+        i2c.writeByte(0x2C,0x4C,LOWER);
+        i2c.writeByte(0x2C,0x4E,LOWER);
+        i2c.writeByte(0x2C,0x50,LOWER);
+        i2c.writeByte(0x2C,0x52,LOWER);
+        i2c.writeByte(0x2C,0x54,LOWER);
+        i2c.writeByte(0x2C,0x56,LOWER);
         TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x45)));
         TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x47)));
         TEMPLIMIT.push(decodeTEMP(i2c.readByte(0x2C,0x49)));
--- test-suite/unit/FanController/FanController.js
+++ test-suite/unit/FanController/FanController.js
@@ -36,16 +36,186 @@ describe('Testing Protolobe Class', function () {
     });
 });
 */
+/*
 "use strict";
 
 var FanController = require('../../../modules/FanController/FanController.js');
 var unittest = new FanController();
 var temp;
+var UPPER_TEMP = 128,LOWER_TEMP = -128;
+var value;
 
 describe('Fan Controller Class',function(){
     describe('readTemp(CMD_ADDR);', function(){
         it('should recieve byte information for temperature',function(){
-            expect(readTemp()).to.be.a('array');
+            expect(unittest.readTemp()).to.be.a('array');
+        });
+        it('should have 10 values in the array',function(){
+            expect(unittest.readTemp()).to.have.length(11);
+        });
+    });
+    describe('decodeTEMP(temp);', function(){
+        it('should properly convert byte temp to Celius form',function(){
+            temp = '0x7D';
+            expect(unittest.decodeTEMP(temp)).to.be.equal(125);
+        });
+        it('should properly convert byte temp to Celius form',function(){
+            temp = '0x19';
+            expect(unittest.decodeTEMP(temp)).to.be.equal(25);
+        });
+        it('should properly convert byte temp to Celius form',function(){
+            temp = '0x00';
+            expect(unittest.decodeTEMP(temp)).to.be.equal(0);
+        });
+        it('should properly convert byte temp to Celius form',function(){
+            temp = '0xFF';
+            expect(unittest.decodeTEMP(temp)).to.be.equal(-1);
+        });
+        it('should properly conver btye temp to Celius form', function(){
+            temp = '0x80';
+            expect(unittest.decodeTEMP(temp)).to.be.equal(-128);
+        });
+    });
+    describe('writeTempLimits(UPPER_TEMP, LOWER_TEMP)',function(){
+        it('should send data as a byte',function(){
+            expect(unittest.writeTempLimits(UPPER_TEMP,LOWER_TEMP)).to.be.a('array');
+        });
+        it('should return an array of lenth 20',function(){
+            expect(unittest.writeTempLimits(UPPER_TEMP,LOWER_TEMP)).to.have.length(20);
+        });
+        it('values 0-9 should be equal to UPPER_TEMP',function(){
+            var TEMPLIMITS = unittest.writeTempLimits(UPPER_TEMP,LOWER_TEMP);
+            var foo = true;
+            for(i=0;i<10;i++){
+                if(TEMPLIMITS[i]!==UPPER_TEMP){
+                    foo = false;
+                }
+            }
+            expect(foo).to.be.true;
+        });
+        it('values 10-19 should be equal to LOWER_TEMP',function(){
+            var TEMPLIMITS = unittest.writeTempLimits(UPPER_TEMP,LOWER_TEMP);
+            var foo = true;
+            for(i=10;i<20;i++){
+                if(TEMPLIMITS[i]!==LOWER_TEMP){
+                    foo = false;
+                }
+            }
+            expect(foo).to.be.true;
+        });
+    });
+    describe('codeTemp(value)',function(){
+        it('should properly convert Celius to a byte', function(){
+            temp = 0;
+            expect(unittest.codeTemp(temp)).to.be.a(byte);
+        });
+        it('should properly convert byte temp to Celius form',function(){
+            temp = 0;
+            expect(unittest.codeTEMP(temp)).to.be.equal(0x00);
+        });
+        it('should properly convert byte temp to Celius form',function(){
+            temp = 25;
+            expect(unittest.codeTEMP(temp)).to.be.equal(0x19);
+        });
+        it('should properly convert byte temp to Celius form',function(){
+            temp = 125;
+            expect(unittest.codeTEMP(temp)).to.be.equal(0x7D);
+        });
+        it('should properly convert byte temp to Celius form',function(){
+            temp = -10;
+            expect(unittest.codeTEMP(temp)).to.be.equal(0xF6);
+        });
+        it('should properly conver btye temp to Celius form', function(){
+            temp = -128;
+            expect(unittest.codeTEMP(temp)).to.be.equal(0x80);
+        });
+    });
+    describe('setRegister()',function(){
+        it('should return an array',function(){
+            expect(unittest.setRegister()).to.be.a('array');
+        });
+        it('array should be length _____',function(){
+            expect(unittest.setRegister()).to.have.length(1); // I dont know the length yet
+        });
+    });
+    describe('editRegister(CMD_ADDR,value)',function(){
+        it('should ',function(){
+            expect(unittest.editRegister(value)).to.be.a('byte');
+        });
+    });
+    describe('readRegister(CMD_ADDR)',function(){
+        it('should read a byte value', function(){
+            expect(unittest.readRegister(CMD_ADDR)).to.be.a('byte');
+        });
+    });
+}); */
+"use strict";
+
+var file = require('../../../modules/FanController/FanController.js');
+var expect = require('chai').expect;
+var temp;
+    var dev_addr1 =[], dev_addr2 =[];
+    var register1 =[], register2 =[];
+    var command1 =[], command2 =[];
+    class i2c_bus{
+        constructor(something1, something2, something3) {
+            
+        }
+        writeByteSync(dev_addr, register, command){
+            dev_addr1.push(dev_addr);
+            register1.push(register);
+            command1.push(command);
+            
+        }
+        readByteSync(dev_addr, register){
+            return register;
+        }
+        writeByte(dev_addr, register, command, cb){
+            dev_addr2.push(dev_addr);
+            register2.push(register);
+            command2.push(command);
+        }
+        readByte(dev_addr, register, cb){
+
+        }
+        reset(){
+            dev_addr1 =[], dev_addr2 =[];
+            register1 =[], register2 =[];
+            command1 =[], command2 =[];
+        }
+    };
+
+var i2c = new i2c_bus();
+var model = function(){};
+var log = function() { }
+    log.output = function(input) { 
+        expected_log = "";
+        for (var i = 0; i < arguments.length; i++) {
+            if(typeof arguments[i] === "object") {
+                expected_log += JSON.stringify(arguments[i])+"\n";
+            } else {
+                expected_log += arguments[i];
+            }
+        }
+    };
+
+    var feedback = function(input) { 
+        expected_feedback = "";
+        for (var i = 0; i < arguments.length; i++) {
+            if(typeof arguments[i] === "object") {
+                expected_feedback += JSON.stringify(arguments[i])+"\n";
+            } else {
+                expected_feedback += arguments[i];
+            }
+        } 
+    };
+var unittest = new file("Fan Controller", feedback,log,4000,i2c,model);
+
+describe('Fan Controller Class',function(){
+    describe('readTemp();', function(){
+        unittest.readTemp();
+        it('should recieve byte information for temperature',function(){
+            expect(unittest.TEMP[0]).to.be.a(175);
         });
         it('should have 10 values in the array',function(){
             expect(readTemp()).to.have.length(11);
