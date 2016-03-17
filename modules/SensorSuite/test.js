6cc913a0c6268ab56bbf5578f625ae27aa753c08
kevinserver <kevinserver@yahoo.com>
2016-03-17 15:08:13 -0700

    fjkdshk

---
+++ modules/SensorSuite/test.js
@@ -0,0 +1,12 @@
+var mpu6050 = require('./MPU6050.js');
+var i2c_bus = require('i2c_bus');
+//var config = require('./config.json');
+var log = function() {};
+var i2c = i2c_bus.openSync(1);
+var mpu = new mpu6050(i2c, log);
+mpu.wakeUp();
+mpu.readData();
+mpu.convertPosition();
+mpu.convettTemp();
+mpu.Log();
+mpu.sleep();
