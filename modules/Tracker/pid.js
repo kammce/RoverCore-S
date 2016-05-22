b86a4572e45c1e03213c8a4174c6fa822b43cb27
John Strube <jwstrube@gmail.com>
2016-05-21 20:15:28 -0700

    Replaced PID with weighted moving average

--- modules/Tracker/pid.js
+++ modules/Tracker/pid.js
@@ -8,13 +8,15 @@ class pidController {
 		this.Kd = Kd;
 		this.dt = dt;
 		this.target = 0;
-		this.output = 0;
+		//this.output = 0;
 		this.current = 0;
 		this.err = 0;
 		this.err0 = 0;
 		this.errSum = 0;
 		this.errDiff = 0;	
 
+		this.output = 0;
+		this.weight = .2;
 	}
 
 	setTarget(value) {
@@ -22,12 +24,13 @@ class pidController {
 	}
 
 	update(current, target) {
+		/*
 		this.current = current;
 		this.target = target;
 
 		this.err0 = this.err;
 		this.err = this.target - this.current;
-		this.errSum = this.errSum + this.err*this.dt;
+		this.errSum = (this.errSum + this.err)*this.dt;
 		this.errDiff = (this.err - this.err0)/this.dt;
 
 		if(this.errSum > 1) {
@@ -37,7 +40,9 @@ class pidController {
 		}
 
 		return (this.Kp * this.err) + (this.Ki * this.errSum) + (this.Kd*this.errDiff);
-
+		*/
+		this.output = this.weight*target+(1-this.weight)*this.output;		
+		return this.output;
 	}
 
 	setPID(Kp, Ki, Kd) {
