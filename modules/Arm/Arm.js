cef92d9d2c76efefeb375ec542c62fb4de0892eb
kammce <khalil.estell@sjsu.edu>
2016-05-30 14:17:44 -0700

    Updating scientic

---
+++ modules/Arm/Arm.js
@@ -0,0 +1,111 @@
+"use strict";
+
+var Neuron = require('../Neuron');
+
+class Arm extends Neuron {
+	constructor(util) {
+		super(util);
+		this.name = util.name;
+		this.feedback = util.feedback;
+		this.log = util.log;
+		this.idle_time = util.idle_timeout;
+		this.i2c = util.i2c;
+		this.model = util.model;
+
+		var parent = this;
+
+		const RETRY_LIMIT = 50;
+		const INTERVAL_TIME = 100;
+		const SETUP_TIME = 5000;
+		const ARM_PATH = "COM16";
+		const SERVO_OFF = 5;
+		var trys = 0;
+
+        this.model.registerMemory('Arm');
+
+		////////////////
+		this.arm = {
+			rotonda: 500,
+			base: 500,
+			elbow: 500,
+			method: 0,
+			pitch: 0,
+			roll: 0,
+			claw: 999,
+			laser: 0,
+		};
+		/////////////////
+		this.port = new util.serial.SerialPort(ARM_PATH, {
+			baudrate: 9600,
+			parser: util.serial.parsers.readline('\n')
+		}, false); // false = disable auto open
+
+		/* Serial Open Routine: will continously attempt to access the
+		 * serialport until retry limit has been met. */
+		var serialOpenRoutine = (err) => {
+			if(trys >= RETRY_LIMIT) {
+				return;
+			} else if (err) {
+				this.log.output(`Failed to open ${ARM_PATH}`, trys);
+				this.feedback(`Failed to open ${ARM_PATH}`);
+				trys++;
+				setTimeout(() => {
+					this.log.output(`Reattempting to open ${ARM_PATH}`, trys);
+					this.feedback(`Reattempting to open ${ARM_PATH}`);
+					this.port.open(serialOpenRoutine);
+				}, 2000);
+				return;
+			} else {
+				// Listen for data on the serial port
+				this.port.on('data', (data) => {
+					this.log.output("RECIEVED " + data.toString());
+				});
+				// Handle Error events by sending them back to mission control
+				this.port.on("err", (err) => {
+					this.log.output("Communication error with /dev/ArmMCU");
+					this.feedback("Communication error with /dev/ArmMCU");
+				});
+			}
+		};
+		// Attempt to open Serial port
+		this.port.open(serialOpenRoutine);
+	}
+	map(input, i_min, i_max, o_min, o_max) {
+		var output = (input - i_min)*(o_max - o_min)/(i_max-i_min)+o_min;
+		if(output > o_max) { return Math.round(o_max); }
+		if(output < o_min) { return Math.round(o_min); }
+		return Math.round(output);
+	}
+	react(input) {
+		this.arm.rotonda = this.map(input.rotonda, -360, 360, 1, 999); // THIS IS STILL WRONG! THE DIRECTION IS 180 OFF!
+		this.arm.base = this.map(input.base, -90, 270, 1, 999);
+		var elbow = Math.abs(360-input.base);
+		this.arm.elbow = this.map(input.elbow, 1, 360, 1, 999); // 0 is down, 999 is up
+		var pitch_angle = 270-(90-input.pitch); // [-90 (pitch up), 90, (pitch down)]
+		this.arm.pitch = this.map(pitch_angle, 1, 270, 1, 270);
+		this.arm.method = input.method; // is what it is
+		this.arm.roll = this.map(input.roll, 1, 360, 1, 360); // used to clamp
+		this.arm.claw = this.map(input.claw, 1, 999, 1, 999); // used to clamp
+		this.arm.laser = input.laser;
+
+		// C:999,500,500,180,1,180,500,1E
+		var command = `C:${this.arm.rotonda},${this.arm.base},${this.arm.elbow},${this.arm.pitch},${this.arm.method},${this.arm.roll},${this.arm.claw},${this.arm.laser}E\n`;
+		//this.log.output("C:999,500,500,180,1,180,500,1E\n");
+		//this.port.write("C:999,500,500,180,1,180,500,1E\n");
+		this.log.output(command);
+		this.port.write(command);
+
+		this.model.set('ArmPosition', command);
+	}
+	halt() {
+		if(this.port.isOpen()) {
+			//this.port.write('');
+		}
+		this.log.output(`HALTING ${this.name}`);
+		this.feedback(this.name ,`HALTING ${this.name}`);
+	}
+	resume() {}
+	idle() {}
+}
+
+module.exports = Arm;
--- modules/Scientific/Scientific.js
+++ modules/Scientific/Scientific.js
@@ -1,259 +1,95 @@
-// // type   name    function   library
-// var serialport = require('serialport');   
-// var SerialPort = serialport.SerialPort;
-// //var prompt = require('prompt');
-//   //prompt.start();
-
-//   //user enters a third argument, being the serialport
-//   var portName = "/dev/ttyUSB0";
-
-
-// //arduino object created with
-// var port = new SerialPort(portName,
-// {
-//   //speed             parser
-//   baudRate:1200,    
-//   parser:serialport.parsers.readline("\r\n")
-// });
-
-
-
-
-// var degrees;
-
-// port.on('open', onOpen);
-// port.on('data', onData);
-
-// function onOpen(){
-// 	console.log("Open sesame");  
-// }
-
-// function onData(data){
-// 	if (parseFloat(data) !== 'NaN')
-// 	{console.log(parseFloat(data));}
-// }
-
-// function end(){
-// 	console.log("Goodbye");
-// 	return 0;
-// }
-
 "use strict";
 
 var Neuron = require('../Neuron');
-var serialport = require('serialport');   
-var SerialPort = serialport.SerialPort;
-var prompt = require('prompt');
-
+var fs = require('fs');
 
 class Scientific extends Neuron {
-	constructor(name, feedback, color_log, idle_timeout, i2c, model) {
-		super(name, feedback, color_log, idle_timeout);
-		this.name = name;
-		this.feedback = feedback;
-		this.log = color_log;
-		this.idle_time = idle_timeout;
-		this.i2c = i2c;
-		this.model = model;
-		this.portName = "/dev/ttyUSB0";
-		this.portName2 = "/dev/ttyUSB0";
-		var OnorOff = "off";
-		var parent = this;        
-		var SerialPort = serialport.SerialPort;
-		var dataContainer = [];
-		var average=0;
-		var samplesize=0;
-		var sum = 0;
-		var port = new SerialPort(this.portName,
+	constructor(util) {
+		super(util);
+		this.name = util.name;
+		this.feedback = util.feedback;
+		this.log = util.log;
+		this.idle_time = util.idle_timeout;
+		this.i2c = util.i2c;
+		this.model = util.model;
 
-		{
-         //speed             parser
-         baudRate:1200,    
-         parser:serialport.parsers.readline("\r\n")
-     });
+		const SENSOR_PATH = "/dev/ttySCIENCE";
+		const LID_PATH = "/dev/ttyLID";
 
-		var port2 = new SerialPort(this.portName,
+		this.model.registerMemory("SENSORS");
 
-		{
-         //speed             parser
-         baudRate:1200,    
-         parser:serialport.parsers.readline("\r\n")
-     });
-        // Construct Class here
+		this.model.set("SENSORS", {
+			moisture: 0,
+			temperature: 0,
+		});
 
+		// Name of stdout file
+		var output_file = `${__dirname}/science-${Date().slice(0,-15)}.log`.replace(/[ :]/g, '-');
+		// Generate the output file along with a write stream to it
+		this.writeScience = fs.createWriteStream(output_file);
 
-    this.scienceOn1 = function(sample){
-        	samplesize = sample;
-        	dataContainer.length=0;
-        	sum = 0;
-        	OnorOff = "on1";
-        // getPrompt();
-        
-        console.log(OnorOff);
+		this.sensor_port = new SerialPort(SENSOR_PATH, {
+			baudRate: 1200,
+			parser:serialport.parsers.readline("\r\n")
+		});
+		this.lid_port = new util.serial.SerialPort(LID_PATH, {
+			baudrate: 9600,
+			parser: util.serial.parsers.readline('\n')
+		}); // false = disable auto open
 
-    }
+		this.writeScience.write('datetime, temperature, moisture');
 
-    this.scienceOn2 = function(sample){
-        	samplesize = sample;
-        	dataContainer.length=0;
-        	sum = 0;
-        	OnorOff = "on2";
-        // getPrompt();
-        
-        console.log(OnorOff);
-
-    }
-
-    this.scienceOff = function(){
-    	OnorOff = "off";
-    }
-
-    this.onOpen = function onOpen(){
-    	console.log("Open sesame"); 
-    }
-
-    this.onData1 = function (data){
-    	if (parseFloat(data) != 'NaN' && OnorOff == "on1")
-    	{
-
-    		if(samplesize == 0)console.log(parseFloat(data));
-
-    		else
-    		{
-
-    			sum = sum + parseFloat(data);
-    			dataContainer.push(parseFloat(data));
-
-    			if(dataContainer.length == samplesize)
-    			{
-    				average = sum/samplesize;
-                    // var variance = 0;
-                    // for (var i = 0; i < samplesize; i++) {
-                    //     variance += ((average - dataContainer[i])*(average - dataContainer[i]));
-                    // }
-
-                    // variance = variance/samplesize;
-                    // var deviation = Math.sqrt(variance);
-
-                    // console.log("average: " + average.toFixed(2) + "\tstandard deviation: " + deviation.toFixed(2));
-                    console.log("average: " + average.toFixed(2));
-                    dataContainer.length=0;
-                    sum = 0;
-                }
-                
-
-            }
-        }
-    }
-
-    this.onData2 = function (data){
-    	if (parseFloat(data) != 'NaN' && OnorOff == "on2")
-    	{
-
-    		if(samplesize == 0)console.log(parseFloat(data));
-
-    		else
-    		{
-
-    			sum = sum + parseFloat(data);
-    			dataContainer.push(parseFloat(data));
-
-    			if(dataContainer.length == samplesize)
-    			{
-    				average = sum/samplesize;
-                    // var variance = 0;
-                    // for (var i = 0; i < samplesize; i++) {
-                    //     variance += ((average - dataContainer[i])*(average - dataContainer[i]));
-                    // }
-
-                    // variance = variance/samplesize;
-                    // var deviation = Math.sqrt(variance);
-
-                    // console.log("average: " + average.toFixed(2) + "\tstandard deviation: " + deviation.toFixed(2));
-                    console.log("average: " + average.toFixed(2));
-                    dataContainer.length=0;
-                    sum = 0;
-                }
-                
-
-            }
-        }
-    }
-
-    port.on('open', this.onOpen1);
-    port.on('data', this.onData1);
-    port2.on('open', this.onOpen2);
-    port2.on('data', this.onData2);
-
-
-}
-
-
-react(input) {
-	var sensor = input.sensor;
-	var state = input.state;
-	var sample = input.sample;
-
-	switch(sensor)
-	{
-		case "Temperature":
-		{
-			if(state=="on")
-			{
-				console.log("Temperature sensor with sample size "+sample+":\n");
-				this.scienceOn1(sample);
+		this.sensor_port.on('data', (data) => {
+			var current_memory = this.model.get("SENSORS");
+			if(/T[0-9\.]/g.test(data)) {
+				var temp = parseFloat(data.substring(1));
+				if(!isNaN(temp)) {
+					current_memory['temperature'] = temp;
+					this.model.set("SENSORS", current_memory);
+					this.writeScience.write(`${Date().slice(0,-15)},${current_memory['temperature']}, ${current_memory['moisture']}`);
+				}
+			} else if(/M[0-9\.]/g.test(data)) {
+				var temp = parseFloat(data.substring(1));
+				if(!isNaN(temp)) {
+					current_memory['moisture'] = temp;
+					this.model.set("SENSORS", current_memory);
+					this.writeScience.write(`${Date().slice(0,-15)},${current_memory['temperature']}, ${current_memory['moisture']}`);
+				}
 			}
-			else if(state == "off")
-			{
-				console.log("Off");
-				this.scienceOff();
-			}
-			else console.log("Invalid input");
+		});
+	}
 
-			break;
+	react(input) {
+		switch(input)
+		{
+			case "LID_OPEN":
+				if(this.lid_port.isOpen()) {
+					this.lid_port.write("1\r\n");
+				}
+				break;
+			case "LID_CLOSE":
+				if(this.lid_port.isOpen()) {
+					this.lid_port.write("0\r\n");
+				}
+				break;
+			default:
+				this.log.output("invalid lid command sent")
+				break;
 		}
+	}
 
-		case "Moisture":
-		{
-			if(state=="on")
-			{
-				console.log("Moisture sensor with sample size "+sample+":\n");
-				this.scienceOn2(sample);
-			}
-			else if(state == "off")
-			{
-				console.log("Off");
-				this.scienceOff();
-			}
-			else console.log("Invalid input");
-
-			break;
-		}
-
-		default:
-		console.log("Tool does not exist");
-		break;
+	halt() {
+		this.log.output(`HALTING ${this.name}`);
+		this.feedback(this.name ,`HALTING ${this.name}`);
+	}
+	resume() {
+		this.log.output(`RESUMING ${this.name}`);
+		this.feedback(this.name ,`RESUMING ${this.name}`);
+	}
+	idle() {
+		this.log.output(`IDLING ${this.name}`);
+		this.feedback(this.name ,`IDLING ${this.name}`);
 	}
 }
-
-halt() {
-	this.log.output(`HALTING ${this.name}`);
-	this.feedback(this.name ,`HALTING ${this.name}`);
-}
-resume() {
-	this.log.output(`RESUMING ${this.name}`);
-	this.feedback(this.name ,`RESUMING ${this.name}`);
-}
-idle() {
-	this.log.output(`IDLING ${this.name}`);
-	this.feedback(this.name ,`IDLING ${this.name}`);
-}
-}
-
-//var swag = new Scientific();
-// swag.scienceOn();
-
-
-
 
 module.exports = Scientific;
