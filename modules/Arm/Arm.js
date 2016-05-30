9f03dcca60303a63bf8ce992da3ce51e782ee7f0
kammce <khalil.estell@sjsu.edu>
2016-05-30 14:45:30 -0700

    fixing merge conflicts with arm

--- modules/Arm/Arm.js
+++ modules/Arm/Arm.js
@@ -11,13 +11,13 @@ class Arm extends Neuron {
 		this.idle_time = util.idle_timeout;
 		this.i2c = util.i2c;
 		this.model = util.model;
-		
+
 		var parent = this;
-		
+
 		const RETRY_LIMIT = 50;
 		const INTERVAL_TIME = 100;
 		const SETUP_TIME = 5000;
-		const ARM_PATH = "/dev/ttyArm";
+		const ARM_PATH = "/dev/ttyARM";
 		const SERVO_OFF = 5;
 		var trys = 0;
 
@@ -28,9 +28,9 @@ class Arm extends Neuron {
 			rotonda: 500,
 			base: 500,
 			elbow: 500,
-			wrist_method: SERVO_OFF,
-			wrist_pitch: 0,
-			wrist_roll: 0,
+			method: 0,
+			pitch: 0,
+			roll: 0,
 			claw: 999,
 			laser: 0,
 		}
@@ -40,12 +40,12 @@ class Arm extends Neuron {
 			parser: util.serial.parsers.readline('\n')
 		}, false); // false = disable auto open
 
-		/* Serial Open Routine: will continously attempt to access the 
+		/* Serial Open Routine: will continously attempt to access the
 		 * serialport until retry limit has been met. */
 		var serialOpenRoutine = (err) => {
 			if(trys >= RETRY_LIMIT) {
 				return;
-			} else if (err) { 
+			} else if (err) {
 				this.log.output(`Failed to open ${ARM_PATH}`, trys);
 				this.feedback(`Failed to open ${ARM_PATH}`);
 				trys++;
@@ -58,7 +58,7 @@ class Arm extends Neuron {
 			} else {
 				// Listen for data on the serial port
 				this.port.on('data', (data) => {
-					this.log.output("RECIEVED" + data.toString());
+					this.log.output("RECIEVED " + data.toString());
 				});
 				// Handle Error events by sending them back to mission control
 				this.port.on("err", (err) => {
@@ -77,18 +77,24 @@ class Arm extends Neuron {
 		return Math.round(output);
 	}
 	react(input) {
-		this.arm.rotonda = this.map(input.rotonda, -360, 360, 0, 999); // THIS IS STILL WRONG! THE DIRECTION IS 180 OFF!
-		this.arm.base = this.map(input.base, -90, 270, 0, 999); 
-		this.arm.elbow = this.map(input.elbow, 0, 360, 0, 999); // 0 is up, 999 is down
-		this.arm.wrist_method = input.method; // is what it is
-		this.arm.wrist_pitch = 270-(90-input.pitch); // [-90 (pitch up), 90, (pitch down)]
-		this.arm.wrist_roll = this.map(input.roll, 0, 360, 0, 360); // used to clamp
-		this.arm.claw = this.map(input.claw, 0, 999, 0, 999); // used to clamp
+		this.arm.rotonda = this.map(input.rotonda, -360, 360, 1, 999); // THIS IS STILL WRONG! THE DIRECTION IS 180 OFF!
+		this.arm.base = this.map(input.base, -90, 270, 1, 999);
+		var elbow = Math.abs(360-input.base);
+		this.arm.elbow = this.map(input.elbow, 1, 360, 1, 999); // 0 is down, 999 is up
+		var pitch_angle = 270-(90-input.pitch); // [-90 (pitch up), 90, (pitch down)]
+		this.arm.pitch = this.map(pitch_angle, 1, 270, 1, 270);
+		this.arm.method = input.method; // is what it is
+		this.arm.roll = this.map(input.roll, 1, 360, 1, 360); // used to clamp
+		this.arm.claw = this.map(input.claw, 1, 999, 1, 999); // used to clamp
 		this.arm.laser = input.laser;
 
 		//C:999,500,500,180,1,180,500,1E
-		var command = `C:${this.arm.rotonda},${this.arm.base},${this.arm.elbow},${this.arm.wrist_method},${this.arm.wrist_pitch},${this.arm.wrist_roll},${this.arm.claw},${this.arm.laser}E\n`;
+		var command = `C:${this.arm.rotonda},${this.arm.base},${this.arm.elbow},${this.arm.pitch},${this.arm.method},${this.arm.roll},${this.arm.claw},${this.arm.laser}E\n`;
+		//this.log.output("C:999,500,500,180,1,180,500,1E\n");
+		//this.port.write("C:999,500,500,180,1,180,500,1E\n");
+		this.log.output(command);
 		this.port.write(command);
+
 		this.model.set('ArmPosition', command);
 	}
 	halt() {
