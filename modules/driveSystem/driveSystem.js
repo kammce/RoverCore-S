8b6830bf1d0f4733bc062b109b7fc8cbd846b024
kammce <khalil.estell@sjsu.edu>
2016-05-30 19:25:17 -0700

    Fixin' up some drivesystem

--- modules/driveSystem/driveSystem.js
+++ modules/driveSystem/driveSystem.js
@@ -17,16 +17,16 @@ class DriveSystem extends Neuron {
         this.idle_time = util.idle_timeout;
         this.i2c = util.i2c;
         this.model = util.model;
-        
+
         var parent = this;
-        
+
         const retryLimit = 50;
         const INTERVAL_TIME = 100;
         const SETUP_TIME = 5000;
         var trys = 0;
 
-        /* sendState: will send goal states to the FIST-MCU 
-         * (Feedback influenced Steering and Travel system) */ 
+        /* sendState: will send goal states to the FIST-MCU
+         * (Feedback influenced Steering and Travel system) */
         var sendState = function () {
             if(parent.port.isOpen()) {
                 if(parent.mode !== parent.modeOld){
@@ -62,27 +62,28 @@ class DriveSystem extends Neuron {
         this.limitOld = 50;
         this.PIDState = 'on';
         this.PIDStateOld = 'off';
-        this.mode = 'z'; 
+        this.mode = 'z';
         // z is arbitrary, and forces sendState to send a k
         this.modeOld = 'z';
         /////////////////
-        this.port = new util.serial.SerialPort("/dev/FIST-MCU", {
+        const FIST_PATH = "/dev/ttyFIST";
+        this.port = new util.serial.SerialPort(FIST_PATH, {
             baudrate: 9600,
             parser: util.serial.parsers.readline('\n')
         }, false); // false = disable auto open
 
-        /* Serial Open Routine: will continously attempt to access the 
+        /* Serial Open Routine: will continously attempt to access the
          * serialport until retry limit has been met. */
         var serialOpenRoutine = (err) => {
             if(trys >= retryLimit) {
                 return;
-            } else if (err) { 
-                this.log.output("Failed to open /dev/FIST-MCU", trys);
-                this.feedback("Failed to open /dev/FIST-MCU");
+            } else if (err) {
+                this.log.output(`Failed to open ${FIST_PATH}`, trys);
+                this.feedback(`Failed to open ${FIST_PATH}`);
                 trys++;
                 setTimeout(() => {
-                    this.log.output("Reattempting to open /dev/FIST-MCU", trys);
-                    this.feedback("Reattempting to open /dev/FIST-MCU");
+                    this.log.output(`Reattempting to open ${FIST_PATH}`, trys);
+                    this.feedback(`Reattempting to open ${FIST_PATH}`);
                     this.port.open(serialOpenRoutine);
                 }, 2000);
                 return;
@@ -105,106 +106,37 @@ class DriveSystem extends Neuron {
         });
     }
     react(input) {
-        //if(this.state === 'react'){
-            this.log.output("React was called");
-            this.log.output("input = ", input);
-            this.speed = input.speed;
-            this.angle = input.angle;
-            this.mode = input.mode;
-            this.limit = input.limit;
-            this.PIDState = input.PIDState;
-            this.log.output(`REACTING ${this.name}: `, input);
-            this.feedback(this.name ,`REACTING ${this.name}: `, input);
-            //console.log("DATA that was Recieved" + input.toString());
-            //this.port.write(input.toString());
-            //this.port.write("\n");
-            /*return true;
-
-        }
-        else{
-            return false;
-        }*/
+        this.speed = input.speed;
+        this.angle = input.angle;
+        this.mode = input.mode;
+        this.limit = input.limit;
+        this.PIDState = input.PIDState;
+        this.log.output(`REACTING ${this.name}: `, input);
+        this.feedback(this.name ,`REACTING ${this.name}: `, input);
     }
     halt() {
-        this.state = 'halt';
         if(this.port.isOpen()) {
             this.port.write('S000,090E');
         }
         this.speed = 0;
+        this.angle = 90;
         this.log.output(`HALTING ${this.name}`);
         this.feedback(this.name ,`HALTING ${this.name}`);
     }
     resume() {
-        this.state = 'react';
-        //this.interval = setInterval(this.sendState(), 100);
         this.log.output(`RESUMING ${this.name}`);
         this.feedback(this.name ,`RESUMING ${this.name}`);
     }
     idle() {
-        this.state = 'idle';
         if(this.port.isOpen()) {
             this.port.write('S000,090E');
         }
         this.speed = 0;
-        //clearInterval(this.interval);
+        this.angle = 90;
         this.log.output(`IDLING ${this.name}`);
         this.feedback(this.name ,`IDLING ${this.name}`);
-        //this.port.write('S000,090E');
-                
+
     }
 }
-/*var dataSplit;
- this.cur=['a','b','c','d','e','f'];
-        this.rpm=['a','b','c','d','e','f'];
-                    if(data[0] === 'r' && data.includes('\n')){
-                        dataSplit = data.split(",");
-                         parent.rpm.a = parseInt(['0x' + dataSplit[1]]);
-                         parent.rpm.b = parseInt(['0x' + dataSplit[2]]);
-                         parent.rpm.c = parseInt(['0x' + dataSplit[3]]);
-                         parent.rpm.d = parseInt(['0x' + dataSplit[4]]);
-                         parent.rpm.e = parseInt(['0x' + dataSplit[5]]);
-                         parent.rpm.f = parseInt(['0x' + dataSplit[6]]);
-                    }
-                    else if(data[0] === 'c' && data.includes('\n')){
-                         dataSplit = data.split(",");
-                         parent.cur.a = parseInt(['0x' + dataSplit[1]])/100;
-                         parent.cur.b = parseInt(['0x' + dataSplit[2]])/100;
-                         parent.cur.c = parseInt(['0x' + dataSplit[3]])/100;
-                         parent.cur.d = parseInt(['0x' + dataSplit[4]])/100;
-                         parent.cur.e = parseInt(['0x' + dataSplit[5]])/100;
-                         parent.cur.f = parseInt(['0x' + dataSplit[6]])/100;
-                    }
-                    else {
-                        console.log("Data Error!");
-                    }
-
-                    else{
-            this.port = port;
-            this.port.on('data', function (data){
-                var dataSplit;
-                if(data[0] === 'r' && data.includes('\n')){
-                    dataSplit = data.split(",");
-                     this.rpm.a = parseInt(['0x' + dataSplit[1]]);
-                     this.rpm.b = parseInt(['0x' + dataSplit[2]]);
-                     this.rpm.c = parseInt(['0x' + dataSplit[3]]);
-                     this.rpm.d = parseInt(['0x' + dataSplit[4]]);
-                     this.rpm.e = parseInt(['0x' + dataSplit[5]]);
-                     this.rpm.f = parseInt(['0x' + dataSplit[6]]);
-                }
-                else if(data[0] === 'c' && data.includes('\n')){
-                     dataSplit = data.split(",");
-                     this.cur.a = parseInt(['0x' + dataSplit[1]])/100;
-                     this.cur.b = parseInt(['0x' + dataSplit[2]])/100;
-                     this.cur.c = parseInt(['0x' + dataSplit[3]])/100;
-                     this.cur.d = parseInt(['0x' + dataSplit[4]])/100;
-                     this.cur.e = parseInt(['0x' + dataSplit[5]])/100;
-                     this.cur.f = parseInt(['0x' + dataSplit[6]])/100;
-                }
-                else {
-                    console.log("Data Error!");
-                }
-            });
-            this.interval = setInterval(sendState, 100);
-        }*/
 
 module.exports = DriveSystem;
