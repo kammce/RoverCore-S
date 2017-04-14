5e2bcf59f4dab15029f8301285c35bcd146908fc
Khalil Estell <kammcecorp@gmail.com>
2017-04-14 21:58:09 +0000

    Merged in kammce/cortex (pull request #47)
    Kammce/cortex
    
    * All RoverCore unit tests pass. Upcall feature has been added and 80% tested (did not test system shutdown or rovercore restart).
    
    * Added installer script. Installs NodeJS, npm, npm packages, grunt-cli and mocha cli
    
    * Added a section of the script that keeps user from running it as root
    
    Approved-by: Khalil Estell <kammcecorp@gmail.com>

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+function SystemExit()
+{
+    echo "Exiting Script" 1>&2
+    exit 1
+}
+
+# Make sure only non-root users can run our script
+if [[ $EUID -eq 0 ]]; then
+   echo -n "RoverCoreV2 installer script must NOT be run as root! " 1>&2
+   echo -n "." 1>&2
+   sleep 1
+   echo -n "." 1>&2
+   sleep 1
+   echo -n "." 1>&2
+   sleep 1
+   echo " So try again!" 1>&2
+   exit 1
+fi
+
+# Commented out because 2017 rover does not use the udev rules
+# echo "Installing rover udev rules (video and bluetooth identifiers) into /etc/udev/rules.d"
+# cp install/udev-rules/rover.rules /etc/udev/rules.d
+
+echo "Need sudo privileges to run script."
+sudo echo "" || SystemExit
+
+echo -e "\nInstalling Build Essentials"
+sudo apt-get install -y build-essential
+
+# Adding Node Source Repository to Apt-Get
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+
+echo -e "\nInstalling NodeJS"
+sudo apt-get install -y nodejs
+
+echo -e "\nInstall RoverCore NPM Dependencies"
+npm install .
+echo -e "\nNPM Install Grunt Command Line Interface"
+sudo npm install -g grunt-cli
+echo -e "\nNPM Install Mocha Command Line Interface"
+sudo npm install -g mocha-cli
+
+# Kill sudo timestamp
+sudo -k
