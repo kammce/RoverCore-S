86f8fb3c4fc84177001efe7ef087bb55cd6cb9c6
Rolando Javier <rjavier441@gmail.com>
2017-05-06 21:47:42 -0700

    Updates to support Bang Bang Control.

--- modules/Tracker/Tracker.js
+++ modules/Tracker/Tracker.js
@@ -60,44 +60,49 @@ class Tracker extends Neuron
 
 		/* Constants/Definitions */
 		// Teensy Keys for key:value pairs to/from bluetooth
-		this.LOCAL_ORIENTATION_X = 'a'; 	// "97";
-		this.LOCAL_ORIENTATION_Y = 'b';		// "98";
-		this.LOCAL_ORIENTATION_Z = 'c'; 	// "99";
-		this.GLOBAL_ORIENTATION_X = 'd'; 	// "100";
-		this.GLOBAL_ORIENTATION_Y = 'e';	// "101";
-		this.GLOBAL_ORIENTATION_Z = 'f';	// "102";
+		this.RELATIVE_ORIENTATION_X = 'a'; 	// "97";	// float roll ~0, relative to rover,
+		this.RELATIVE_ORIENTATION_Y = 'b';	// "98";	// float pitch: relative to rover, +90 = straight up, -90 = straight down
+		this.RELATIVE_ORIENTATION_Z = 'c'; 	// "99";	// float yaw: relative to rover, 0 = forward, +-360 = all the forward (- = left, + = right)  
+		this.GLOBAL_ORIENTATION_X = 'd'; 	// "100";	// float roll: No on cares
+		this.GLOBAL_ORIENTATION_Y = 'e';	// "101";	// float pitch: relative to the Earth's horizon
+		this.GLOBAL_ORIENTATION_Z = 'f';	// "102";	// float heading (relative direction to Earth's Magnetic North)
 		this.LIDAR_READING = 'g';			// "103";
 		this.YAW_MOTOR_CURRENT = 'h';		// "104";
 		this.PITCH_MOTOR_CURRENT = 'i';		// "105";
-		this.MOTION_CONTROL_MODE = 'A';		// "65"; key for specifying speed/dir control (val = 1) or position control (val = 2)
-		this.MOTION_COMMAND_YAW = 'B';		// "66"; key for specifying input to yaw motor (signed)
-		this.MOTION_COMMAND_PITCH = 'C';	// "67"; key for specifying input to pitch motor (signed)
-		this.ACTIVE_CAMERA = 'D';			// "68"; key for selecting which analog camera to receive feed from
-		this.BATTERY_VOLTAGE = 'E';			// "69";
+		// this.MOTION_CONTROL_MODE = 'A';		// "65"; key for specifying speed/dir control (val = 1) or position control (val = 2)
+		this.MOTION_COMMAND_YAW = 'B';		// "66"; key for specifying angle to yaw motor (signed)
+		this.MOTION_COMMAND_PITCH = 'C';	// "67"; key for specifying angle to pitch motor (signed)
+		this.MOTION_COMMAND_YAW_SPEED = 'D';// "68";	// 0 - 255
+		this.MOTION_COMMAND_PITCH_SPEED = 'E';// "69";	// 0 - 255
+		this.ACTIVE_CAMERA = 'F';	// "?" key for selecting which analog camera to receive feed from
+		this.BATTERY_VOLTAGE = 'G';
 
 		/* Bluetooth Serial */
-		this.comms = new util.extended.BluetoothSerial(
-			{
-				mac: "21:13:710e",
-				baud: 38400,	//recommended baud rate
-				log: this.log,
-				device: 3,		//tracker = device 3
-				callback: (data) =>
-				{
-					this.log.output(data);
-				}
-			}
-		);
+		this.comms = new util.extended.BluetoothSerial({
+			mac: "00:21:13:00:3B:03",
+			baud: 38400,
+			log: this.log,
+			device: 3
+		});
+
+		// /* USB Serial */
+		// this.comms = new util.extended.Serial(
+		// 	{
+		// 		log: this.log,
+		// 		path: "tty/ACM0",
+		// 		baud: 38400
+		// 	}
+		// );
 
 		/* Locals */
 		this.local = {
 			busy: false,		// prevents command bottle-necking
-			orientation: {		// orientation relative to Rover
+			relativeOr: {		// orientation relative to Rover
 				X: 0,
 				Y: 0,
 				Z: 0
 			},
-			heading: {			// orientation relative to Earth
+			globalOr: {			// orientation relative to Earth
 				X: 0,
 				Y: 0,
 				Z: 0
@@ -108,15 +113,15 @@ class Tracker extends Neuron
 				pitch: 0
 			},
 			lidarState: false,	// LIDAR activation (currently not implemented; lidar is assumed always on)
-			ctlMode: "SD",		// Control Mode; SD: Speed/Direction Control, P: Position control
-			preset: "Cancel",	// Preset (if any): for values, see MC/Tracker lobe comm. standards
+			// ctlMode: "SD",		// Control Mode; SD: Speed/Direction Control, P: Position control
+			// preset: "Cancel",	// Preset (if any): for values, see MC/Tracker lobe comm. standards
 			pitch: {			// Pitch parameters
 				speed: 0,
-				direction: "up"
+				angle: 0
 			},
 			yaw: {				// Yaw parameters
 				speed: 0,
-				direction: "left"
+				angle: 0
 			},
 			zoom: 0			// Zoom percentage
 		};
@@ -126,23 +131,23 @@ class Tracker extends Neuron
 		this.model.set("Tracker", this.local);	// set "Tracker" to local
 
 		/* Teensy Data Listeners (to read and asynchronously update vars) */
-		this.comms.attachListener(this.LOCAL_ORIENTATION_X, (val) => {
-			this.local.orientation.X = val;
+		this.comms.attachListener(this.RELATIVE_ORIENTATION_X, (val) => {
+			this.local.relativeOr.X = val;
 		});
-		this.comms.attachListener(this.LOCAL_ORIENTATION_Y, (val) => {
-			this.local.orientation.Y = val;
+		this.comms.attachListener(this.RELATIVE_ORIENTATION_Y, (val) => {
+			this.local.relativeOr.Y = val;
 		});
-		this.comms.attachListener(this.LOCAL_ORIENTATION_Z, (val) => {
-			this.local.orientation.Z = val;
+		this.comms.attachListener(this.RELATIVE_ORIENTATION_Z, (val) => {
+			this.local.relativeOr.Z = val;
 		});
 		this.comms.attachListener(this.GLOBAL_ORIENTATION_X, (val) => {
-			this.local.heading.X = val;
+			this.local.globalOr.X = val;
 		});
 		this.comms.attachListener(this.GLOBAL_ORIENTATION_Y, (val) => {
-			this.local.heading.Y = val;
+			this.local.globalOr.Y = val;
 		});
 		this.comms.attachListener(this.GLOBAL_ORIENTATION_Z, (val) => {
-			this.local.heading.Z = val;
+			this.local.globalOr.Z = val;
 		});
 		this.comms.attachListener(this.LIDAR_READING, (val) => {
 			this.local.distance = val;
@@ -167,17 +172,17 @@ class Tracker extends Neuron
 			this.local.busy = true;
 
 			// Determine Control Mode
-			var tempCtlMode = this.getControlMode(input);
-			if(tempCtlMode === false)
-			{
-				this.log.output("Error", "Ambiguous control mode");
-				reset();
-				return false;
-			}
-			else
-			{
-				this.local.ctlMode = tempCtlMode;
-			}
+			// var tempCtlMode = this.getControlMode(input);
+			// if(tempCtlMode === false)
+			// {
+			// 	this.log.output("Error", "Ambiguous control mode");
+			// 	reset();
+			// 	return false;
+			// }
+			// else
+			// {
+			// 	this.local.ctlMode = tempCtlMode;
+			// }
 
 			// Determine Preset (if any)
 			if((Object.keys(input)).indexOf("preset") !== -1)
@@ -189,51 +194,68 @@ class Tracker extends Neuron
 			// Prepare parameters to Teensy (BluetoothSerial) and Mission Control (Model)
 
 			// Pitch/Yaw parameters
-			switch(tempCtlMode)
-			{
-				case "SD":
-				{
-					// Pass SPEED/DIRECTIONAL Control Parameters
-					this.local.pitch = {
-						speed: input.pitch.speed,
-						direction: input.pitch.direction
-					};
-					this.local.yaw = {
-						speed: input.yaw.speed,
-						direction: input.yaw.direction
-					};
-					// this.log.output(`speed = ${input.yaw.speed}`);	// an example of using ECMA script 6
+			// Pass SPEED/DIRECTIONAL Control Parameters
+			this.local.pitch = {
+				speed: input.pitch.speed,
+				angle: input.pitch.angle
+			};
+			this.local.yaw = {
+				speed: input.yaw.speed,
+				angle: input.yaw.angle
+			};
+			// this.log.output(`speed = ${input.yaw.speed}`);	// an example of using ECMA script 6
 
-					// Send to Teensy (i.e. pitch -8.45 = CCW 8.45, yaw 12.4 = CW 12.4)
-					this.comms.send(this.MOTION_COMMAND_PITCH,
-						(this.local.pitch.direction === "up") ? this.local.pitch.speed : (this.local.pitch.speed * (-1))
-					);
-					this.comms.send(this.MOTION_COMMAND_YAW,
-						(this.local.yaw.direction === "right") ? this.local.yaw.speed: (this.local.yaw.speed * (-1))
-					);
-					break;
-				}
-				case "P":
-				{
-					// Pass POSITIONAL Control Parameters
-					this.local.pitch = {
-						angle: input.pitch.angle
-					};
-					this.local.yaw = {
-						angle: input.yaw.angle
-					};
+			// Send to Teensy (i.e. pitch -8.45 = CCW 8.45, yaw 12.4 = CW 12.4)
+			this.comms.send(this.MOTION_COMMAND_PITCH, this.local.pitch.angle);
+			this.comms.send(this.MOTION_COMMAND_PITCH_SPEED, this.local.pitch.speed);
+			this.comms.send(this.MOTION_COMMAND_YAW, this.local.yaw.angle);
+			this.comms.send(this.MOTION_COMMAND_YAW_SPEED, this.local.yaw.speed);
 
-					// Send to Teensy
-					this.comms.send(this.MOTION_COMMAND_PITCH, input.pitch.angle);
-					this.comms.send(this.MOTION_COMMAND_YAW, input.yaw.angle);
-					break;
-				}
-				default:
-				{
-					this.log.output("Default", "Doing Nothing...");
-					break;
-				}
-			}
+			// switch(tempCtlMode)
+			// {
+			// 	case "SD":
+			// 	{
+			// 		// Pass SPEED/DIRECTIONAL Control Parameters
+			// 		this.local.pitch = {
+			// 			speed: input.pitch.speed,
+			// 			direction: input.pitch.direction
+			// 		};
+			// 		this.local.yaw = {
+			// 			speed: input.yaw.speed,
+			// 			direction: input.yaw.direction
+			// 		};
+			// 		// this.log.output(`speed = ${input.yaw.speed}`);	// an example of using ECMA script 6
+
+			// 		// Send to Teensy (i.e. pitch -8.45 = CCW 8.45, yaw 12.4 = CW 12.4)
+			// 		this.comms.send(this.MOTION_COMMAND_PITCH,
+			// 			(this.local.pitch.direction === "up") ? this.local.pitch.speed : (this.local.pitch.speed * (-1))
+			// 		);
+			// 		this.comms.send(this.MOTION_COMMAND_YAW,
+			// 			(this.local.yaw.direction === "right") ? this.local.yaw.speed: (this.local.yaw.speed * (-1))
+			// 		);
+			// 		break;
+			// 	}
+			// 	case "P":
+			// 	{
+			// 		// Pass POSITIONAL Control Parameters
+			// 		this.local.pitch = {
+			// 			angle: input.pitch.angle
+			// 		};
+			// 		this.local.yaw = {
+			// 			angle: input.yaw.angle
+			// 		};
+
+			// 		// Send to Teensy
+			// 		this.comms.send(this.MOTION_COMMAND_PITCH, input.pitch.angle);
+			// 		this.comms.send(this.MOTION_COMMAND_YAW, input.yaw.angle);
+			// 		break;
+			// 	}
+			// 	default:
+			// 	{
+			// 		this.log.output("Default", "Doing Nothing...");
+			// 		break;
+			// 	}
+			// }
 
 			// Zoom parameters
 			this.local.zoom = input.zoom;
--- utilities/extended/BluetoothSerial.js
+++ utilities/extended/BluetoothSerial.js
@@ -1,157 +1,487 @@
+// "use strict";
+
+// var Serial = require('./Serial');
+
+// class BluetoothSerial extends Serial
+// {
+// 	constructor(params)
+// 	{
+// 		params.path = `/dev/rfcomm${params.device}`;
+// 		super(params);
+
+// 		this.device = params.device;
+// 		this.mac_address = params.mac;
+// 		this.serial_buffer = "";
+// 		this.callback_map = {};
+
+// 		this.bind();
+// 	}
+// 	bind()
+// 	{
+// 		this.exec(`rfcomm bind ${this.device} ${this.mac_address}`, (error, stdout, stderr) =>
+// 		{
+// 			if (error)
+// 			{
+// 				this.log.output(`Bluetooth Serial RFCOMM BIND error: ${error}`);
+// 			}
+// 			else
+// 			{
+// 				this.setupSerial();
+// 			}
+// 		});
+// 	}
+// 	//// @Override Superclass
+// 	onPortData(data)
+// 	{
+// 		this.blue = this.blue || this;
+
+// 		this.blue.serial_buffer += data.toString();
+// 		var messages = this.blue.serial_buffer.split('\r\n');
+// 		//// Check if messages contains something
+// 		if(messages.length > 1)
+// 		{
+// 			for (var i = 0; i < messages.length-1; i++)
+// 			{
+// 				/* Regex pattern for format @<key>,<value>
+// 				 * Store 1st match in key
+// 				 * Store 2nd match in value
+// 				 * Return empty array if exec fails to find matches
+// 				 * In the event of a failed match, key & value = undefined
+// 				 */
+// 				var [, key, value] = /^@([a-zA-Z]),([0-9\-]+)$/g.exec(messages[i]) || [];
+// 				/* Check if there exists a callback for this key.
+// 				 * Check will fail if regex match failed.
+// 				 * 		key is undefined, thus typeof will return "undefined".
+// 				 */
+// 				if(typeof this.callback_map[key] === "function")
+// 				{
+// 					value = parseFloat(value);
+// 					this.blue.callback_map[key](value);
+// 				}
+// 				else
+// 				{
+// 					this.blue.log.output("ERROR: COULD NOT BLUETOOTHSERIAL CALL FUNCTION HANDLER FOR 'KEY' = ", key);
+// 				}
+// 			}
+// 			this.blue.serial_buffer = messages[messages.length-1];
+// 		}
+// 	}
+// 	sendCommand(key, value)
+// 	{
+// 		var msg = `@${key.charAt(0)},${parseFloat(value)}\r\n`;
+// 		this.send(msg);
+// 	}
+// 	attachListener(key, callback)
+// 	{
+// 		if(/^[a-zA-Z]$/g.test(key) && typeof callback === 'function')
+// 		{
+// 			this.callback_map[key] = callback;
+// 			return true;
+// 		}
+// 		return false;
+// 	}
+// }
+
+
+// BluetoothSerial.bt_agent_process = undefined;
+
+// //// For BlueZ5 bt-agent, for BlueZ4 bluetooth-agent
+// //// NOTE: Bluetooth-agent has different arguments
+// BluetoothSerial.bluetooth_agent = "bt-agent";
+// BluetoothSerial.bluetooth_pincode_path = "/tmp/BluetoothPincodes";
+
+// // "MAC Address": passkey/pin
+// BluetoothSerial.bluetooth_devices = `
+// 00:21:13:00:71:0e 1234
+// 00:21:13:00:6e:a7 1234
+// 00:21:13:00:72:ba 1234
+// 00:21:13:00:71:a1 1234
+// 00:21:13:00:6f:a7 1234
+// 00:21:13:00:71:57 1234
+// `;
+
+// BluetoothSerial.spawnBTAgent = function(agent_ps, code_path)
+// {
+// 	var spawn = require("child_process").spawn;
+
+// 	BluetoothSerial.bt_agent_process = spawn(agent_ps,
+// 	[
+// 		"--capability", "NoInputNoOutput",
+// 		"--pin", code_path
+// 	]);
+// 	BluetoothSerial.bt_agent_process.on('close', (code) =>
+// 	{
+// 		//// NOTE: Could be potentially dangerous :P
+// 		//// Recursion mang!
+// 		console.log("bt-agent (Bluetooth Pincode Pairing Agent) closed! RESTARTING in 1s!");
+// 		setTimeout(() =>
+// 		{
+// 			BluetoothSerial.spawnBTAgent(
+// 				BluetoothSerial.bluetooth_agent,
+// 				BluetoothSerial.bluetooth_pincode_path
+// 			);
+// 		}, 1000);
+// 	});
+// };
+
+// BluetoothSerial.initialize = function()
+// {
+// 	var execSync = require("child_process").execSync;
+// 	var fs = require("fs");
+
+// 	//// Kill all rfcomm processes before proceeding
+// 	try { execSync('killall -9 rfcomm'); } catch(e) {}
+// 	//// bt-agent requires two SIGTERM signals to terminate fully.
+// 	//// 1st SIGTERM unregisters agent
+// 	try { execSync('killall bt-agent'); } catch(e) {}
+// 	//// 2nd SIGTERM kills bt-agent
+// 	try { execSync('killall bt-agent'); } catch(e) {}
+// 	//// 3rd Just to make sure
+// 	try { execSync('killall bt-agent'); } catch(e) {}
+// 	//// Release all bluetooth rfcomm connections
+// 	try { execSync('rfcomm release all'); } catch(e) {}
+
+// 	fs.writeFileSync(
+// 		BluetoothSerial.bluetooth_pincode_path,
+// 		BluetoothSerial.bluetooth_devices
+// 	);
+
+// 	BluetoothSerial.spawnBTAgent(
+// 		BluetoothSerial.bluetooth_agent,
+// 		BluetoothSerial.bluetooth_pincode_path
+// 	);
+// };
+
+// BluetoothSerial.initialize();
+
+// module.exports = BluetoothSerial;
+
+
+
+
+
+
+
+
+
+
+
 "use strict";
+
+
 
 var Serial = require('./Serial');
 
+
+
 class BluetoothSerial extends Serial
+
 {
+
 	constructor(params)
+
 	{
+
 		params.path = `/dev/rfcomm${params.device}`;
+		console.log("testing");
+
 		super(params);
 
+
+
 		this.device = params.device;
+
 		this.mac_address = params.mac;
+
 		this.serial_buffer = "";
+
 		this.callback_map = {};
 
+
+
 		this.bind();
+
 	}
+
 	bind()
+
 	{
+
 		this.exec(`rfcomm bind ${this.device} ${this.mac_address}`, (error, stdout, stderr) =>
+
 		{
+
 			if (error)
+
 			{
+
 				this.log.output(`Bluetooth Serial RFCOMM BIND error: ${error}`);
+
 			}
+
 			else
+
 			{
+
 				this.setupSerial();
+
 			}
+
 		});
+
 	}
+
 	//// @Override Superclass
+
 	onPortData(data)
+
 	{
 		this.reference = this.reference || this;
 
 		this.reference.serial_buffer += data.toString();
 		var messages = this.reference.serial_buffer.split('\r\n');
 		//// Check if messages contains something
+
 		if(messages.length > 1)
+
 		{
+
+			// this.reference.log.output(messages);
+
 			for (var i = 0; i < messages.length-1; i++)
+
 			{
+
 				/* Regex pattern for format @<key>,<value>
+
 				 * Store 1st match in key
+
 				 * Store 2nd match in value
+
 				 * Return empty array if exec fails to find matches
+
 				 * In the event of a failed match, key & value = undefined
+
 				 */
-				var [, key, value] = /^@([a-zA-Z]),([0-9\-]+)$/g.exec(messages[i]) || [];
+
+				var map = /^@([a-zA-Z0-9]),([\.\-0-9]+)$/g.exec(messages[i]) || [];
+
+				if(map.length !== 3) 
+
+				{ 
+
+					//this.log.output(`FAILED ON =${messages[i]}=`);
+
+					continue; 
+
+				}
+
+				var [, key, value] = map;
+
 				/* Check if there exists a callback for this key.
+
 				 * Check will fail if regex match failed.
+
 				 * 		key is undefined, thus typeof will return "undefined".
+
 				 */
 				if(typeof this.reference.callback_map[key] === "function")
 				{
+
 					value = parseFloat(value);
 					this.reference.callback_map[key](value);
 				}
+
 				else
+
 				{
 					this.reference.log.output("ERROR: COULD NOT BLUETOOTHSERIAL CALL FUNCTION HANDLER FOR 'KEY' = ", key);
 				}
+
 			}
 			this.reference.serial_buffer = messages[messages.length-1];
 		}
+
 	}
+
 	sendCommand(key, value)
+
 	{
+
 		var msg = `@${key.charAt(0)},${parseFloat(value)}\r\n`;
+
 		this.send(msg);
+
+		this.log.output(msg);
+
 	}
+
 	attachListener(key, callback)
+
 	{
-		if(/^[a-zA-Z]$/g.test(key) && typeof callback === 'function')
+
+		if(/^[a-zA-Z0-9]$/g.test(key) && typeof callback === 'function')
+
 		{
+
 			this.callback_map[key] = callback;
+
 			return true;
+
 		}
+
 		return false;
+
 	}
+
 }
+
+
+
 
 
 BluetoothSerial.bt_agent_process = undefined;
 
+
+
 //// For BlueZ5 bt-agent, for BlueZ4 bluetooth-agent
+
 //// NOTE: Bluetooth-agent has different arguments
+
 BluetoothSerial.bluetooth_agent = "bt-agent";
+
 BluetoothSerial.bluetooth_pincode_path = "/tmp/BluetoothPincodes";
 
+
+
 // "MAC Address": passkey/pin
+
 BluetoothSerial.bluetooth_devices = `
+
 00:21:13:00:71:0e 1234
+
 00:21:13:00:6e:a7 1234
-00:21:13:00:72:ba 1234
+
+00:21:13:00:3b:03 1234
+
 00:21:13:00:71:a1 1234
+
 00:21:13:00:6f:a7 1234
+
 00:21:13:00:71:57 1234
+
 `;
 
+
+
 BluetoothSerial.spawnBTAgent = function(agent_ps, code_path)
+
 {
+
 	var spawn = require("child_process").spawn;
 
+
+
 	BluetoothSerial.bt_agent_process = spawn(agent_ps,
+
 	[
+
 		"--capability", "NoInputNoOutput",
+
 		"--pin", code_path
+
 	]);
+
 	BluetoothSerial.bt_agent_process.on('close', (code) =>
+
 	{
+
 		//// NOTE: Could be potentially dangerous :P
+
 		//// Recursion mang!
+
 		console.log("bt-agent (Bluetooth Pincode Pairing Agent) closed! RESTARTING in 1s!");
+
+
+
 		setTimeout(() =>
+
 		{
+
 			BluetoothSerial.spawnBTAgent(
+
 				BluetoothSerial.bluetooth_agent,
+
 				BluetoothSerial.bluetooth_pincode_path
+
 			);
+
 		}, 1000);
+
 	});
+
 };
+
+
 
 BluetoothSerial.initialize = function()
+
 {
+
 	var execSync = require("child_process").execSync;
+
 	var fs = require("fs");
 
+
+
 	//// Kill all rfcomm processes before proceeding
+
 	try { execSync('killall -9 rfcomm'); } catch(e) {}
+
 	//// bt-agent requires two SIGTERM signals to terminate fully.
+
 	//// 1st SIGTERM unregisters agent
+
 	try { execSync('killall bt-agent'); } catch(e) {}
+
 	//// 2nd SIGTERM kills bt-agent
+
 	try { execSync('killall bt-agent'); } catch(e) {}
+
 	//// 3rd Just to make sure
+
 	try { execSync('killall bt-agent'); } catch(e) {}
+
 	//// Release all bluetooth rfcomm connections
+
 	try { execSync('rfcomm release all'); } catch(e) {}
 
+
+
 	fs.writeFileSync(
+
 		BluetoothSerial.bluetooth_pincode_path,
+
 		BluetoothSerial.bluetooth_devices
+
 	);
 
-	BluetoothSerial.spawnBTAgent(
-		BluetoothSerial.bluetooth_agent,
-		BluetoothSerial.bluetooth_pincode_path
-	);
+
+
+	// BluetoothSerial.spawnBTAgent(
+
+	// 	BluetoothSerial.bluetooth_agent,
+
+	// 	BluetoothSerial.bluetooth_pincode_path
+
+	// );
+
 };
 
+
+
 BluetoothSerial.initialize();
+
+
 
 module.exports = BluetoothSerial;
